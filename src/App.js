0848e7797e1743c338ff24c25bcd15db9c0c683c
cintia_sestelo <cintia.sestelo@sieve.com.br>
2018-10-26 10:32:32 -0300

    Adiciona lógica do relógio e de mudança de cor de fundo baseado na hora atual

--- src/App.js
+++ src/App.js
@@ -13,33 +13,55 @@ class App extends Component {
 
     componentDidMount() {
         setInterval(() => {
-            if (this.state.sec < 59) {
-                this.setState({sec: this.state.sec + 1})
-            } else {
-                this.setState({sec: 0})
-            }
+            this.updateClockState();
         }, 1000)
     }
 
+    updateClockState() {
+        if (this.state.sec < 59) {
+            this.setState({sec: this.state.sec + 1})
+        } else {
+            this.setState({sec: 0});
+            if (this.state.min < 59) {
+                this.setState({min: this.state.min + 1})
+            } else {
+                this.setState({min: 0});
+                if (this.state.hour < 23) {
+                    this.setState({hour: this.state.hour + 1})
+                } else {
+                    this.setState({hour: 0, min: 0, sec: 0})
+                }
+            }
+        }
+    }
+
+    calculateColour(time, timeMaxValue) {
+        return ((time * 255) / timeMaxValue).toFixed()
+    }
+
+    getColour() {
+        const hour = this.calculateColour(this.state.hour, 23);
+        const min = this.calculateColour(this.state.min, 59);
+        const sec = this.calculateColour(this.state.sec, 59);
+
+        return `RGB(${hour}, ${min}, ${sec})`
+    }
+
     render() {
+        const hour = String(this.state.hour).padStart(2, '0');
+        const min = String(this.state.min).padStart(2, '0');
+        const sec = String(this.state.sec).padStart(2, '0');
+
         return (
             <div className="App">
-                <header className="App-header">
+                <header className="App-header" style={{backgroundColor: this.getColour()}}>
                     <p>
-                        {this.state.hour}:{this.state.min}:{this.state.sec}
+                        {hour}:{min}:{sec}
+                    </p>
+                    <p>
+                        {this.getColour()}
                     </p>
                     <img src={logo} className="App-logo" alt="logo"/>
-                    <p>
-                        Edit <code>src/App.js</code> and save to reload.
-                    </p>
-                    <a
-                        className="App-link"
-                        href="https://reactjs.org"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                    >
-                        Learn React
-                    </a>
                 </header>
             </div>
         );
