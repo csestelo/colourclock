f5c6ccabc7db25418855353012383f690f8a68bb
csestelo <sestelos@gmail.com>
2018-11-21 00:10:30 -0200

    adiciona primeiro teste (muitos fogos)

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import {shallow} from 'enzyme';
 
 it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+    const div = document.createElement('div');
+    ReactDOM.render(<App/>, div);
+    ReactDOM.unmountComponentAtNode(div);
+});
+
+describe('ClockComponent', () => {
+    var MockDate = require('mockdate');
+
+    beforeEach(() => {
+        var MockDate = require('mockdate');
+        MockDate.set('2018/1/1 00:00:00')
+    });
+
+    afterEach(() => {
+        MockDate.reset();
+    });
+
+    test('renders current time when initialize', () => {
+        const wrapper = shallow(<App/>);
+        expect(wrapper.text()).toBe('00:00:00RGB(0, 0, 0)')
+    })
 });
