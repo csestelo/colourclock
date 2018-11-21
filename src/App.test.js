04378e9c9904274aaf1d18c8e210f046704396d7
csestelo <sestelos@gmail.com>
2018-11-21 00:33:52 -0200

    tentando mockar setInterval

--- src/App.test.js
+++ src/App.test.js
@@ -14,14 +14,22 @@ describe('ClockComponent', () => {
 
     beforeEach(() => {
         var MockDate = require('mockdate');
-        MockDate.set('2018/1/1 00:00:00')
+        MockDate.set('2018/1/1 00:00:00');
+        jest.useFakeTimers();
     });
 
     afterEach(() => {
         MockDate.reset();
+        jest.clearAllTimers()
     });
 
-    test('renders current time when initialize', () => {
+    test('renders current time and colour when initialize', () => {
+        const wrapper = shallow(<App/>);
+        expect(wrapper.text()).toBe('00:00:00RGB(0, 0, 0)')
+    });
+
+    test('passed 60 seconds increments minute and set colour', () => {
+        jest.advanceTimersByTime(60000);
         const wrapper = shallow(<App/>);
         expect(wrapper.text()).toBe('00:00:00RGB(0, 0, 0)')
     })
